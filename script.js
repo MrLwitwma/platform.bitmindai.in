36a6199ba5f9c352f09739b9592d43a4b5c6cf6f
MrLwitwma <147829440+MrLwitwma@users.noreply.github.com>
2025-03-11 10:59:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.getElementById("apiForm").addEventListener("submit", async function (event) {
+    event.preventDefault();
+    
+    const email = document.getElementById("email").value;
+    if (!email) return alert("Please enter your email.");
+
+    try {
+        const response = await fetch("http://127.0.0.1:5000/v1/api/create/dictionary", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email })
+        });
+
+        const result = await response.json();
+        console.log(result)
+
+        if (response.ok) {
+            document.getElementById("apiKey").innerText = result.api_key;
+            document.getElementById("result").classList.remove("hidden");
+        } else {
+            alert(result.error || "Something went wrong.");
+        }
+    } catch (error) {
+        alert(error)
+        alert("Failed to connect to server.");
+    }
+});
+
+document.getElementById("copyBtn").addEventListener("click", function () {
+    const apiKeyField = document.getElementById("apiKey").textContent;
+    const tempInput = document.createElement("textarea");
+    tempInput.value = apiKeyField;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+    alert("API Key copied!");
+});
+
